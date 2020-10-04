4fab2ca7759dfc3be0d2f226885669c40aaf2b82
Anton Öberg <ao222qg@student.lnu.se>
2020-10-04 21:18:38 +0200

    Add rendering of all values from google spreadsheet.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import path from "path";
 
 //TODO: use service
 import googleSpreadSheetsHelper from './src/helpers/googleSpreadSheetsHelper';
+import { stringify } from "querystring";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,19 +18,29 @@ app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    googleSpreadSheetsHelper.GetSpreadSheetDeveloperValues().then(result =>
-        res.render('index', { 
-            firstNameProp: JSON.parse(result).values[0][0],
-            firstNameVal: JSON.parse(result).values[1][0],
-            lastNameProp: JSON.parse(result).values[0][1],
-            lastNameVal: JSON.parse(result).values[1][1],
-            emailProp: JSON.parse(result).values[0][2],
-            emailVal: JSON.parse(result).values[1][2],
-            githubProp: JSON.parse(result).values[0][3],
-            githubVal: JSON.parse(result).values[1][3],
-            linkedInProp: JSON.parse(result).values[0][4],
-            linkedInVal: JSON.parse(result).values[1][4]
-        }))
+    Promise.all([googleSpreadSheetsHelper.GetSpreadSheetDeveloperValues(), googleSpreadSheetsHelper.GetSpreadSheetWorkplacesValues()])
+        .then(result => 
+                res.render('index', { 
+                    firstNameProp: JSON.parse(result[0]).values[0][0],
+                    firstNameVal: JSON.parse(result[0]).values[1][0],
+                    lastNameProp: JSON.parse(result[0]).values[0][1],
+                    lastNameVal: JSON.parse(result[0]).values[1][1],
+                    emailProp: JSON.parse(result[0]).values[0][2],
+                    emailVal: JSON.parse(result[0]).values[1][2],
+                    githubProp: JSON.parse(result[0]).values[0][3],
+                    githubVal: JSON.parse(result[0]).values[1][3],
+                    linkedInProp: JSON.parse(result[0]).values[0][4],
+                    linkedInVal: JSON.parse(result[0]).values[1][4],
+                    companyNameProp: JSON.parse(result[1]).values[0][0],
+                    companyNameval1: JSON.parse(result[1]).values[1][0],
+                    companyNameval2: JSON.parse(result[1]).values[2][0],
+                    roleProp: JSON.parse(result[1]).values[0][1],
+                    roleVal1: JSON.parse(result[1]).values[1][1],
+                    roleVal2: JSON.parse(result[1]).values[2][1],
+                    timePeriodProp: JSON.parse(result[1]).values[0][2],
+                    timePeriodVal1: JSON.parse(result[1]).values[1][2],
+                    timePeriodVal2: JSON.parse(result[1]).values[2][2]
+                }));
 });
 
 app.all('*', (req, res) => {
